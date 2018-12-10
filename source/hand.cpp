ab9e827e2ebb0207cc82f306612f6893cc962574
Bradley John Krakar <bjk170630@cslinux1.utdallas.edu>
2018-12-10 14:14:43 -0600

    Fixed hand always using the same random number

--- source/hand.cpp
+++ source/hand.cpp
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <gsl/gsl_rng.h>
-
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#include <sys/types.h>
 using namespace std;
 gsl_rng_type *_gsl_rng_type;
 gsl_rng * _gsl_rng;
 int main(int argc, char** argv)
 {
 	getopt(argc, argv, "p:");
-	int p = *optarg;
+	int p = atoi(optarg);
 	_gsl_rng = gsl_rng_alloc(gsl_rng_mt19937);
-	double rand = gsl_rng_uniform(_gsl_rng) * 10.0;
+	time_t result = time(NULL);
+	result += getpid();
+	gsl_rng_set(_gsl_rng,result); 
+	double rand = gsl_rng_uniform(_gsl_rng) * 100.0;
 	gsl_rng_free(_gsl_rng);
+	cout << p << (double)p << rand;
 	if(rand < (double)p)
 		return 1;
 	else
