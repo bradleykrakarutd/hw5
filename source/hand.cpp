1b41b0199254086af3f9e832d30db458bf1287b3
Bradley John Krakar <bjk170630@cslinux1.utdallas.edu>
2018-12-10 13:38:43 -0600

    fixed casting error in hand

--- source/hand.cpp
+++ source/hand.cpp
@@ -8,7 +8,7 @@ gsl_rng * _gsl_rng;
 int main(int argc, char** argv)
 {
 	getopt(argc, argv, "p:");
-	int p = optarg;
+	int p = *optarg;
 	_gsl_rng = gsl_rng_alloc(gsl_rng_mt19937);
 	double rand = gsl_rng_uniform(_gsl_rng) * 10.0;
 	gsl_rng_free(_gsl_rng);
